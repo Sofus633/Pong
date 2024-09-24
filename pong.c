f54326a9566c8181a124fbc12bb42a5f73170867
Sofus633 <105514939+Sofus633@users.noreply.github.com>
2024-09-24 21:49:04 +0200

    Add files via upload
    Not working but work in progress
    - i need to pass to linux so i can install sdl easyly so goodby windows library 
    
    need debuging :/

--- pong.c
+++ pong.c
@@ -2,25 +2,46 @@
 #include <math.h>
 #define TAILLE 10
 #include <windows.h>
+#include <SDL/SDL.h>
+typedef struct{
+	int up;
+	int down;
+	int left;
+	int right;
+}Inputs;
 
 typedef struct{
 	int x;
 	int y;
-	int vector[2];	
+	int vector[2];
+	int radius;	
 }Ball;
 
+typedef struct{
+	int x;
+	int y;
+	int longeur;
+	int largeur;
+	int velocite[2];
+}Rect;
 
 
-void afficher(int screen[TAILLE][TAILLE],int lenght, Ball *ball1);
+void afficher(int screen[TAILLE][TAILLE],int lenght, Ball ball1, Rect rect1, Rect rect2);
+void update(int screen[TAILLE][TAILLE], int lenght, Ball *ball1, Rect *rect1, Rect *rect2);
 void gameloop();
 void moveball(Ball *ball);
 void collition(Ball *ball, int dirrection);
+void rect(Rect *, Rect *);
+
 int main(){
 	int screen[TAILLE][TAILLE] = {0};
 	int lenght = sqrt(sizeof(screen) / sizeof(int));
 	Ball ball1 = { 0, 5, {1, 1}};
-	
-	gameloop(lenght, ball1, screen);
+	Rect rect1 = {0, 3, 2, 1, {0, 0}};
+	Rect rect2 = {TAILLE,3, 2, 1, {0, 0}};
+	Inputs inp = {0, 0, 0, 0}
+
+	gameloop(lenght, ball1, screen, rect1, rect2);
 }
 
 
@@ -51,13 +72,22 @@ void moveball(Ball *ball){
 	printf("%d %d", ball->x, ball->vector[0]);
 }
 
-void afficher(int screen[TAILLE][TAILLE], int lenght, Ball *ball1){
+
+void moverect(Rect *rect1, Rect *rect2){
+	
+	
+
+}
+
+
+
+void afficher(int screen[TAILLE][TAILLE], int lenght, Ball ball1, Rect rect1, Rect rect2){
 system("cls");
 	printf("matrice de taille %d", lenght);	
 	for (int y = 0; y < lenght; y++){
 		printf("\n");
 		for (int x = 0; x < lenght; x++){
-			if (ball1->x == x && ball1->y == y){
+			if (ball1.x == x && ball1.y == y){
 				printf("O ");
 			}
 			else{
@@ -67,16 +97,47 @@ system("cls");
 	}
 }
 
-
-
-
-void gameloop(int lenght,Ball *ball1,int screen[TAILLE][TAILLE]){
+void update(int screen[TAILLE][TAILLE], int lenght, Ball *ball1, Rect *rect1, Rect *rect2) {
+	moverect(rect1, rect2);
 	moveball(ball1);
-	afficher(screen, lenght, ball1);
-	Sleep(1000);
-	gameloop(lenght, ball1, screen);
+	doInputs(rect1, rect2);
 
 }
 
 
+void gameloop(int screen[TAILLE][TAILLE],int lenght,Ball *ball, Rect *rect1, Rect *rect2){
+	afficher(screen, lenght, ball1, rect1, rect2);
+	update(screen[TAILLE][TAILLE], lenght, ball, rect1, rect2);
+	if (app.up){
+		printf("amazing");
+	}
+	Sleep(1000);
+	gameloop(screen[TAILLE][TAILLE], lenght, *ball, *rect1, *rect2);
+
+}
+
+void doInput(void){
+	SDL_Event event;
+	while(SDL_PollEvent(&event)){
+		switch(event.type){
+			case SDL_QUIT:exit(0);break;
+			case SDL_KEYDOWN:doKeyDown(&event.key);break;
+			case SDL_KEYUP:doKeyUp(&event.key);break;
+			default:break;
+		}}}
+void doKeyDown(SDL_KeyboardEvent *event){
+	if(event->repeat==0){
+		if(event->keysym.scancode==SDL_SCANCODE_UP){app.up=1;}
+		if(event->keysym.scancode==SDL_SCANCODE_DOWN){app.down=1;}
+		if(event->keysym.scancode==SDL_SCANCODE_LEFT){app.left=1;}
+		if(event->keysym.scancode==SDL_SCANCODE_RIGHT){app.right=1;}
+	}}
+
+void doKeyUp(SDL_KeyboardEvent *event){
+	if(event->repeat==0){
+		if(event->keysym.scancode==SDL_SCANCODE_UP){app.up=0;}
+		if(event->keysym.scancode==SDL_SCANCODE_DOWN){app.down=0;}
+		if(event->keysym.scancode==SDL_SCANCODE_LEFT){app.left=0;}
+		if(event->keysym.scancode==SDL_SCANCODE_RIGHT){app.right=0;}
+	}}
 
